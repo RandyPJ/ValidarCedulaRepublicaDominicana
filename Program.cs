ebc793618cf979e8d8b71dad14f8fa448e0c9dce
randypj92 <randy_li25@hotmail.com>
2020-06-03 12:53:32 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿public bool CedulaValida(string cedula)
+{
+    bool resultado = false;
+
+    if (string.IsNullOrEmpty(cedula) ||
+        string.IsNullOrWhiteSpace(cedula))
+        return resultado;
+
+    int[] multiplicadores = { 1, 2, 1, 2, 1, 2, 1, 2, 1, 2 };
+    int verificador = 0;
+    int suma = 0;
+
+    if (cedula.Contains("-"))
+        cedula = cedula.Replace("-","");
+
+    _ = int.TryParse(cedula.Substring(cedula.Length - 1), out verificador);
+
+    for (int i = 0; i < cedula.Length - 1; i++)
+    {
+        _ = int.TryParse(cedula[i].ToString(), out int digito);
+        int multiplicacion = digito * multiplicadores[i];
+                
+        if (multiplicacion >= 10)
+        {
+            int primerDigito = (multiplicacion / 10);
+            int segundoDigito = (multiplicacion % 10);
+
+            multiplicacion = primerDigito + segundoDigito;
+        }
+        suma += multiplicacion;
+    }
+
+    if ((suma + verificador) % 10 == 0)
+        resultado = true;
+
+    return resultado;
+}
